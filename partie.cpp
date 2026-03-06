24b21dbb9a84cc4eadf28b8071869d9e576781f2
alice-ch <alice.chakroun123@gmail.com>
2026-03-06 23:26:21 +0100

    feat: cin defensive programming

--- partie.cpp
+++ partie.cpp
@@ -1,171 +1,150 @@
 
 // A besoin de la declaration de la classe
-#include <iostream>
-#include <string>
 #include "partie.hxx"
-#include "plateau.hxx"
-#include "piece.hxx"
 #include "joueur.hxx"
-
+#include "piece.hxx"
+#include "plateau.hxx"
+#include <iostream>
+#include <limits>
+#include <string>
 
 using namespace std;
 
-Partie::Partie()
-{
-  cout << "Constructeur Partie defaut" << endl;
-}
+Partie::Partie() { cout << "Constructeur Partie defaut" << endl; }
 
-Partie::~Partie()
-{
-  cout << "Destructeur Partie" << endl;
-}
+Partie::~Partie() { cout << "Destructeur Partie" << endl; }
 
-
-void Partie::Acquisition(int* x, int* y){
-    cout << "\nX = ";
-    cin >> *x;
+void Partie::Acquisition(int *x, int *y) {
+  cout << "\nX = ";
+  if (cin >> *x) {
     cout << "\nY = ";
-    cin >> *y;
+    if (cin >> *y) {
+    } else {
+      cout << "\n[Erreur] Veuillez entrer des nombres entiers uniquement.";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+  } else {
+    cout << "\n[Erreur] Veuillez entrer des nombres entiers uniquement.";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  }
 }
 
-
-bool Partie::verifCoordonnees(int x, int y){
-   if((x>=1) && (x<=8) && (y>=1) && (y<=8)){
-        return true;
-    }
-    else{
-        cout << "\nCoordonnees incorrectes. Ressaisissez les valeurs :";
-        return false;
-    }
+bool Partie::verifCoordonnees(int x, int y) {
+  if ((x >= 1) && (x <= 8) && (y >= 1) && (y <= 8)) {
+    return true;
+  } else {
+    cout << "\nCoordonnees incorrectes. Ressaisissez les valeurs :";
+    return false;
+  }
 }
 
+// VERIFIER SI PIECE ADVERSAIRE PEUT SE DEPLACER SUR ROI JOUEUR ACTUEL
+// echec = 1; exit function;
 
-    //VERIFIER SI PIECE ADVERSAIRE PEUT SE DEPLACER SUR ROI JOUEUR ACTUEL
-    //echec = 1; exit function;
+int Partie::YaEchecAdv() {
+  int caseEchec = -1;
+  int rbX, rbY, rnX, rnY;
 
-int Partie::YaEchecAdv()
-{
-    int caseEchec = -1;
-    int rbX, rbY, rnX, rnY;
+  // RECUPERER COORDONNEES DES 2 ROIS
+  for (int i = 1; i <= 8; i++) {
+    for (int j = 1; j <= 8; j++) {
+      if (e.getPiece(i, j) != NULL) {
+        // On vérifier si la pièce est un roi, si oui on récupère ses
+        // coordonnées
+        if (e.getPiece(i, j)->codePiece() == 'R') {
+          rbX = i;
+          rbY = j;
+        } else {
+          if (e.getPiece(i, j)->codePiece() == 'r') {
+            rnX = i;
+            rnY = j;
+          }
+        }
+      }
+    }
+  }
 
-    //RECUPERER COORDONNEES DES 2 ROIS
-    for (int i = 1; i <= 8; i++)
-    {
-        for(int j = 1; j <= 8; j++)
+  // VERIFIER SI PIECE JOUEUR ACTUEL PEUT SE DEPLACER SUR ROI ADVERSAIRE
+  for (int k = 1; k <= 8; k++) {
+    for (int l = 1; l <= 8; l++) {
+      if (e.getPiece(k, l) != NULL) {
+        if (joueurActuel) // joueur blanc
         {
-            if(e.getPiece(i, j) != NULL)
-            {
-                //On vérifier si la pièce est un roi, si oui on récupère ses coordonnées
-                if (e.getPiece(i, j)->codePiece() == 'R')
-                {
-                    rbX = i;
-                    rbY = j;
-                }
-                else
-                {
-                    if (e.getPiece(i, j)->codePiece() == 'r')
-                    {
-                        rnX = i;
-                        rnY = j;
-                    }
-                }
-            }
-        }
-    }
+          if (e.getPiece(k, l)->isWhite()) {
+            if (e.getPiece(k, l)->mouvementValide(e, rnX, rnY)) {
+              caseEchec = 2;
 
-    //VERIFIER SI PIECE JOUEUR ACTUEL PEUT SE DEPLACER SUR ROI ADVERSAIRE
-    for (int k = 1; k <= 8; k++){
-        for(int l = 1; l <= 8; l++){
-            if(e.getPiece(k, l) != NULL)
-            {
-                if (joueurActuel) //joueur blanc
-                {
-                    if(e.getPiece(k, l)->isWhite())
-                    {
-                        if(e.getPiece(k, l)->mouvementValide(e, rnX, rnY))
-                        {
-                            caseEchec = 2;
-
-                            //VERIFIER SI ROI ADVERSAIRE PEUT SE DEPLACER
-                            for (int m = rnX-1 ; rnX+1 ; rnX++)
-                            {
-                                for (int n = rnY-1; rnY+1; rnY++)
-                                {
-                                    if (e.getPiece(rnX,rnY)->mouvementValide(e, m, n))
-                                    {
-                                        caseEchec = 3;
-                                        goto fin;
-                                    }
-                                }
-                            }
-                        }
-                    }
+              // VERIFIER SI ROI ADVERSAIRE PEUT SE DEPLACER
+              for (int m = rnX - 1; rnX + 1; rnX++) {
+                for (int n = rnY - 1; rnY + 1; rnY++) {
+                  if (e.getPiece(rnX, rnY)->mouvementValide(e, m, n)) {
+                    caseEchec = 3;
+                    goto fin;
+                  }
                 }
-                else
-                {
-                    if(e.getPiece(k, l)->mouvementValide(e, rbX, rbY))
-                    {
-                        caseEchec = 2;
-                        //VERIFIER SI ROI ADVERSAIRE PEUT SE DEPLACER
-                        for (int m = rnX-1 ; rnX+1 ; rnX++)
-                        {
-                            for (int n = rnY-1; rnY+1; rnY++)
-                            {
-                                if (e.getPiece(rnX,rnY)->mouvementValide(e, m, n))
-                                {
-                                    caseEchec = 3;
-                                    goto fin;
-                                }
-                            }
-                        }
-                    }
-                }
+              }
             }
+          }
+        } else {
+          if (e.getPiece(k, l)->mouvementValide(e, rbX, rbY)) {
+            caseEchec = 2;
+            // VERIFIER SI ROI ADVERSAIRE PEUT SE DEPLACER
+            for (int m = rnX - 1; rnX + 1; rnX++) {
+              for (int n = rnY - 1; rnY + 1; rnY++) {
+                if (e.getPiece(rnX, rnY)->mouvementValide(e, m, n)) {
+                  caseEchec = 3;
+                  goto fin;
+                }
+              }
+            }
+          }
         }
+      }
     }
+  }
 
 fin:
-    return (caseEchec);
+  return (caseEchec);
 }
 
+void Partie::jouerPartie() {
+  /**JoueurBlanc jb = JoueurBlanc();
+  JoueurNoir jn = JoueurNoir();
+  Echiquier e = Echiquier();**/
+  joueurActuel = true; // true = jb, false = jn
+  bool mouvementValide = false;
+  int yaEchec = 0;
+  bool partieFinie = false;
+  int xDep;
+  int yDep;
+  char typePiece;
 
-void Partie::jouerPartie()
-{
-    /**JoueurBlanc jb = JoueurBlanc();
-    JoueurNoir jn = JoueurNoir();
-    Echiquier e = Echiquier();**/
-    joueurActuel = true;    //true = jb, false = jn
-    bool mouvementValide = false;
-    int yaEchec = 0;
-    bool partieFinie = false;
-    int xDep;
-    int yDep;
-    char typePiece;
+  jb.affiche();
+  jn.affiche();
 
-    jb.affiche();
-    jn.affiche();
+  jb.placerPieces(e);
+  jn.placerPieces(e);
 
-    jb.placerPieces(e);
-    jn.placerPieces(e);
-
-    /**CREATION PIECES**/
-  Roi rb(true);                 //ROIS
+  /**CREATION PIECES**/
+  Roi rb(true); // ROIS
   Roi rn(false);
-  Reine qb(true);               //REINES
+  Reine qb(true); // REINES
   Reine qn(false);
-  Tour tb1(true, true);         //TOURS
+  Tour tb1(true, true); // TOURS
   Tour tn1(false, true);
   Tour tb2(true, false);
   Tour tn2(false, false);
-  Fou fb1(true, true);          //FOUS
+  Fou fb1(true, true); // FOUS
   Fou fn1(false, true);
   Fou fb2(true, false);
   Fou fn2(false, false);
-  Cavalier cb1(true, true);     //CAVALIERS
+  Cavalier cb1(true, true); // CAVALIERS
   Cavalier cn1(false, true);
   Cavalier cb2(true, false);
   Cavalier cn2(false, false);
-  Pion pb1(1, true);            //PIONS
+  Pion pb1(1, true); // PIONS
   Pion pn1(1, false);
   Pion pb2(2, true);
   Pion pn2(2, false);
@@ -182,173 +161,160 @@ void Partie::jouerPartie()
   Pion pb8(8, true);
   Pion pn8(8, false);
 
-/**PLACEMENT PIECES**/
-  e.enleverPiece(5,1);  //ROIS
+  /**PLACEMENT PIECES**/
+  e.enleverPiece(5, 1); // ROIS
   e.placer(&rb);
-  e.enleverPiece(5,8);
+  e.enleverPiece(5, 8);
   e.placer(&rn);
-  e.enleverPiece(4,1);  //REINES
+  e.enleverPiece(4, 1); // REINES
   e.placer(&qb);
-  e.enleverPiece(4,8);
+  e.enleverPiece(4, 8);
   e.placer(&qn);
-  e.enleverPiece(1,1);  //TOURS
+  e.enleverPiece(1, 1); // TOURS
   e.placer(&tb1);
-  e.enleverPiece(1,8);
+  e.enleverPiece(1, 8);
   e.placer(&tn1);
-  e.enleverPiece(8,1);
+  e.enleverPiece(8, 1);
   e.placer(&tb2);
-  e.enleverPiece(8,8);
+  e.enleverPiece(8, 8);
   e.placer(&tn2);
-  e.enleverPiece(3,1);  //FOUS
+  e.enleverPiece(3, 1); // FOUS
   e.placer(&fb1);
-  e.enleverPiece(3,8);
+  e.enleverPiece(3, 8);
   e.placer(&fn1);
-  e.enleverPiece(6,1);
+  e.enleverPiece(6, 1);
   e.placer(&fb2);
-  e.enleverPiece(6,8);
+  e.enleverPiece(6, 8);
   e.placer(&fn2);
-  e.enleverPiece(2,1);  //CAVALIERS
+  e.enleverPiece(2, 1); // CAVALIERS
   e.placer(&cb1);
-  e.enleverPiece(2,8);
+  e.enleverPiece(2, 8);
   e.placer(&cn1);
-  e.enleverPiece(7,1);
+  e.enleverPiece(7, 1);
   e.placer(&cb2);
-  e.enleverPiece(7,8);
+  e.enleverPiece(7, 8);
   e.placer(&cn2);
-  e.enleverPiece(1,2);  //PIONS
+  e.enleverPiece(1, 2); // PIONS
   e.placer(&pb1);
-  e.enleverPiece(1,7);
+  e.enleverPiece(1, 7);
   e.placer(&pn1);
-  e.enleverPiece(2,2);
+  e.enleverPiece(2, 2);
   e.placer(&pb2);
-  e.enleverPiece(2,7);
+  e.enleverPiece(2, 7);
   e.placer(&pn2);
-  e.enleverPiece(3,2);
+  e.enleverPiece(3, 2);
   e.placer(&pb3);
-  e.enleverPiece(3,7);
+  e.enleverPiece(3, 7);
   e.placer(&pn3);
-  e.enleverPiece(4,2);
+  e.enleverPiece(4, 2);
   e.placer(&pb4);
-  e.enleverPiece(4,7);
+  e.enleverPiece(4, 7);
   e.placer(&pn4);
-  e.enleverPiece(5,2);
+  e.enleverPiece(5, 2);
   e.placer(&pb5);
-  e.enleverPiece(5,7);
+  e.enleverPiece(5, 7);
   e.placer(&pn5);
-  e.enleverPiece(6,2);
+  e.enleverPiece(6, 2);
   e.placer(&pb6);
-  e.enleverPiece(6,7);
+  e.enleverPiece(6, 7);
   e.placer(&pn6);
-  e.enleverPiece(7,2);
+  e.enleverPiece(7, 2);
   e.placer(&pb7);
-  e.enleverPiece(7,7);
+  e.enleverPiece(7, 7);
   e.placer(&pn7);
-  e.enleverPiece(8,2);
+  e.enleverPiece(8, 2);
   e.placer(&pb8);
-  e.enleverPiece(8,7);
+  e.enleverPiece(8, 7);
   e.placer(&pn8);
 
-/** BOUCLE DE JEU **/
-    while(partieFinie == false)
-    {
-        bool mouvementValide = false;
-        int yaEchec = -1;
+  /** BOUCLE DE JEU **/
+  while (partieFinie == false) {
+    bool mouvementValide = false;
+    int yaEchec = -1;
 
-        changeons_de_piece_a_bouger : e.affiche();    //on affiche le plateau à chaque nouvelle action
+  changeons_de_piece_a_bouger:
+    e.affiche(); // on affiche le plateau à chaque nouvelle action
 
-        /**SAISIE COORDONNEES DE LA PIECE A DEPLACER**/
-        if (joueurActuel)
-        {
-            cout << "\nAu tour du joueur blanc." << endl << "Choisissez les coordonnees de la piece a deplacer.";
-        }
-        else
-        {
-            cout << "\nAu tour du joueur noir." << endl << "Choisissez les coordonnees de la piece a deplacer.";
-        }
-
-        while(!mouvementValide)
-        {
-            Acquisition(&xDep, &yDep);
-            while (!verifCoordonnees(xDep,yDep)){
-                Acquisition(&xDep, &yDep);
-            }
-
-            /**VERIFICATION SI PIECE EXISTE ET SI ELLE EST DE LA COULEUR DU JOUEUR**/
-                if (e.getPiece(xDep, yDep) != NULL)
-                {
-                    if ((joueurActuel && e.getPiece(xDep, yDep)->isWhite()) || (!joueurActuel && e.getPiece(xDep, yDep)->isBlack()))
-                    {
-                        while(!mouvementValide)
-                        {
-                            cout << "\nChoisissez les coordonnees ou vous souhaitez deplacer la piece ou 0 pour quitter : ";
-                            int x;
-                            int y;
-                            Acquisition(&x, &y);
-                            if ( (x == 0) || (y == 0)){
-                                goto changeons_de_piece_a_bouger;
-                            }
-                            while (!verifCoordonnees(x,y)){
-                                Acquisition(&x, &y);
-                            }
-                            if(e.getPiece(xDep,yDep)->mouvementValide(e, x, y))
-                            {
-                                /**VERIFIER SI ECHEC **/
-                                switch(yaEchec)
-                                {
-                                    case 1 :
-                                        cout << "\nDeplacement Invalide : Vous etes en Echec si vous déplacez cette pièce.";
-                                        break;
-                                    case 2 :
-                                        if (joueurActuel == true)
-                                        {
-                                            cout << "\nRoi du Joueur Noir en Echec.";
-                                        }
-                                        else
-                                        {
-                                            cout << "\nRoi du Joueur Blanc en Echec.";
-                                        }
-                                        e.deplacer(e.getPiece(xDep, yDep), x, y);
-                                        mouvementValide = true;
-                                        break;
-                                    case 3 :
-                                        if (joueurActuel == true)
-                                        {
-                                            cout << "\nRoi du Joueur Noir en Echec et Mat.";
-                                        }
-                                        else
-                                        {
-                                            cout << "\nRoi du Joueur Blanc en Echec et Mat.";
-                                        }
-                                        e.deplacer(e.getPiece(xDep, yDep), x, y);
-                                        mouvementValide = true;
-                                        partieFinie = true;
-                                        break;
-                                    default:
-                                        e.deplacer(e.getPiece(xDep, yDep), x, y);
-                                        mouvementValide = true;
-                                        break;
-                                }
-                            }
-                            else
-                            {
-                                cout << "\nCoordonnees invalides pour cette piece.";
-                            }
-                        }
-                    }
-                    else
-                    {
-                        cout << "\nCette piece n'est pas a vous. Choisissez de nouvelles coordonnees :";
-                    }
-                }
-                else
-                {
-                    cout << "\nIl n'y a pas de piece a la position (" << xDep << "," << yDep << ")." << endl << "Choisissez de nouvelles coordonnees :";
-                }
-
-        }
-
-        joueurActuel = !joueurActuel;   //changement de joueur
-
-        //partieFinie = true; //provisoire : afin de ne pas boucler indéfininiment tant que ce n'est pas fini
+    /**SAISIE COORDONNEES DE LA PIECE A DEPLACER**/
+    if (joueurActuel) {
+      cout << "\nAu tour du joueur blanc." << endl
+           << "Choisissez les coordonnees de la piece a deplacer.";
+    } else {
+      cout << "\nAu tour du joueur noir." << endl
+           << "Choisissez les coordonnees de la piece a deplacer.";
     }
+
+    while (!mouvementValide) {
+      Acquisition(&xDep, &yDep);
+      while (!verifCoordonnees(xDep, yDep)) {
+        Acquisition(&xDep, &yDep);
+      }
+
+      /**VERIFICATION SI PIECE EXISTE ET SI ELLE EST DE LA COULEUR DU JOUEUR**/
+      if (e.getPiece(xDep, yDep) != NULL) {
+        if ((joueurActuel && e.getPiece(xDep, yDep)->isWhite()) ||
+            (!joueurActuel && e.getPiece(xDep, yDep)->isBlack())) {
+          while (!mouvementValide) {
+            cout << "\nChoisissez les coordonnees ou vous souhaitez deplacer "
+                    "la piece ou 0 pour quitter : ";
+            int x;
+            int y;
+            Acquisition(&x, &y);
+            if ((x == 0) || (y == 0)) {
+              goto changeons_de_piece_a_bouger;
+            }
+            while (!verifCoordonnees(x, y)) {
+              Acquisition(&x, &y);
+            }
+            if (e.getPiece(xDep, yDep)->mouvementValide(e, x, y)) {
+              /**VERIFIER SI ECHEC **/
+              switch (yaEchec) {
+              case 1:
+                cout << "\nDeplacement Invalide : Vous etes en Echec si vous "
+                        "déplacez cette pièce.";
+                break;
+              case 2:
+                if (joueurActuel == true) {
+                  cout << "\nRoi du Joueur Noir en Echec.";
+                } else {
+                  cout << "\nRoi du Joueur Blanc en Echec.";
+                }
+                e.deplacer(e.getPiece(xDep, yDep), x, y);
+                mouvementValide = true;
+                break;
+              case 3:
+                if (joueurActuel == true) {
+                  cout << "\nRoi du Joueur Noir en Echec et Mat.";
+                } else {
+                  cout << "\nRoi du Joueur Blanc en Echec et Mat.";
+                }
+                e.deplacer(e.getPiece(xDep, yDep), x, y);
+                mouvementValide = true;
+                partieFinie = true;
+                break;
+              default:
+                e.deplacer(e.getPiece(xDep, yDep), x, y);
+                mouvementValide = true;
+                break;
+              }
+            } else {
+              cout << "\nCoordonnees invalides pour cette piece.";
+            }
+          }
+        } else {
+          cout << "\nCette piece n'est pas a vous. Choisissez de nouvelles "
+                  "coordonnees :";
+        }
+      } else {
+        cout << "\nIl n'y a pas de piece a la position (" << xDep << "," << yDep
+             << ")." << endl
+             << "Choisissez de nouvelles coordonnees :";
+      }
+    }
+
+    joueurActuel = !joueurActuel; // changement de joueur
+
+    // partieFinie = true; //provisoire : afin de ne pas boucler indéfininiment
+    // tant que ce n'est pas fini
+  }
 }
